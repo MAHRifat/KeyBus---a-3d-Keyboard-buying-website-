ac68e436044043c2ab8a8500106a1e13da38ddff
AshirRahman <123103184+AshirRahman@users.noreply.github.com>
2025-09-21 14:04:14 +0600

    Start 3d website project

---
+++ src/slices/Hero/index.tsx
@@ -0,0 +1,45 @@
+import { FC } from "react";
+import { Content } from "@prismicio/client";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+
+/**
+ * Props for `Hero`.
+ */
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
+
+/**
+ * Component for "Hero" Slices.
+ */
+const Hero: FC<HeroProps> = ({ slice }) => {
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+      className="text-white blue-gradient-bg relative h-dvh text-shadow-black/30 text-shadow-lg"
+    >
+      <div className="hero-scene pointer-events-none top-0 h-dvh w-full">
+        {/* Canvas goes here */}
+      </div>
+
+      <div className="hero-content absolute inset-x-0 top-0 h-dvh" >
+
+        <PrismicRichText field={slice.primary.heading} components={{
+          heading1: ({ children }) => <h1 className=" hero-heading font-bold-slanted leading-[0.8] sm:text-7xl lg:text-8xl text-7xl uppercase">{children}</h1>,
+
+        }}/>
+        <div className="max-w-md">
+          <PrismicRichText field={slice.primary.body} components={{
+          heading2: ({ children }) => 
+          <h2 className="text-4xl mb-1 uppercase lg:mb-2 lg:text-6xl font-bold-slanted sm:text-5xl">{children}</h2>,
+        }}/>
+        </div>
+        <button className="font-bold-slanted flex w-fit cursor-pointer group gap-1 rounded bg-[#01A7E1] px-3 py-1 text-2xl uppercase transition disable:grayscale">        
+          {slice.primary.buy_button_text}
+          <span className="group-hover:translate-x-1 transition-transform">{">"}</span>
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
